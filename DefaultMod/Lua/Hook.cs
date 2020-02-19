3c5f056ac8c949c31de8f31d425e53648b218ce7
Lil Sizzurp <45602120+LilSizzurp@users.noreply.github.com>
2020-02-19 13:13:55 +0100

    Update Hook.cs
    Adding a hook with the same name and call now replaces it instead of causing an error.

--- DefaultMod/Lua/Hook.cs
+++ DefaultMod/Lua/Hook.cs
@@ -8,7 +8,11 @@ namespace GooseLua.Lua {
         public Dictionary<string, Dictionary<string, Closure>> hooks = new Dictionary<string, Dictionary<string, Closure>>();
         
         public void Add(string hook, string name, Closure action) {
-            hooks[hook].Add(name, action);
+            if(hooks[hook].ContainsKey(name)) {
+                hooks[hook][name] = action;
+            } else {
+                hooks[hook].Add(name, action);
+            }
         }
 
         public void Remove(string hook, string name) {
